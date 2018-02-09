57eaf23466e7145a6c5d13adf0ec7fa028e48f0e
AdamTasma <adamtasma@gmail.com>
2018-02-08 21:42:30 -0500

    first commit

---
+++ Lab15/CountriesApp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class CountriesApp
+    {
+        public static void StartProgram()
+        {
+            Console.WriteLine("What would you like to do? (please enter '1', '2', or '3')");
+            Console.WriteLine("1) List out the current countries.");
+            Console.WriteLine("2) Enter a new country.");
+            Console.WriteLine("3) Exit the program.");
+            UserChooses();
+
+        }
+        public static void UserChooses()
+        {
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        CountriesTextFile.ReadTheFile();
+                        break;
+                    case 2:
+                        CountriesTextFile.WriteTheFile();
+                        break;
+                    case 3:
+                        Console.WriteLine("Goodbye");
+                        break;
+                    default:
+                        Console.WriteLine("that was not a valid option, please re-input.");
+                        UserChooses();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("that was not a valid option, please re-input");
+                UserChooses();
+            }
+        }
+    }
+}
---
+++ Lab15/CountriesTextFile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab15
+{
+    class CountriesTextFile
+    {
+        public static string countriesFilePath = @"C:\Users\adamt\source\repos\Lab15\Lab15\Countries.txt";
+        public static void ReadTheFile()
+        {
+            StreamReader reader = new StreamReader(countriesFilePath, true);
+            //while (true)
+            //{
+            //    string line = reader.ReadLine();
+            //    Console.WriteLine(line);
+            //    if (line == null)
+            //    {
+            //        //Console.WriteLine("Line is null ");
+            //        break;
+            //    }
+
+            //}
+            string text = reader.ReadToEnd();
+            Console.WriteLine("\nCountries List \n--------------");
+            Console.WriteLine(text);
+
+            reader.Close();
+            Console.WriteLine("\n \n Bringing you back to the menu.\n");
+            CountriesApp.StartProgram();
+        }
+
+        public static void WriteTheFile()
+        {
+            Console.WriteLine("Please enter the country to add.");
+            string newCountry = Console.ReadLine();
+
+            //StreamWriter writer = new StreamWriter(countriesFilePath, true);
+            using (StreamWriter writer = File.AppendText(countriesFilePath))
+            {
+                writer.WriteLine(newCountry);
+                writer.Close();
+                Console.WriteLine("\n \n Bringing you back to the menu.\n");
+                CountriesApp.StartProgram();
+            }
+            //Console.WriteLine(writer + newCountry);
+
+
+        }
+    }
+}
